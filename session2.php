835436053039895fbcb4b68acef6ddbba806020e
tesseradecades <nwe8978@rit.edu>
2018-04-14 16:25:37 -0400

    3rd page

--- session2.php
+++ session2.php
@@ -11,7 +11,16 @@ session_start();
 if ($_SESSION["username"] != "") {
 	echo "<h1>Hello ";
     echo $_SESSION["username"];
-    echo "<h2> ";
+	echo "<h2> ";
+?>
+	<br /><br />
+	<a href="clearsessions.php">Clear Sessions</a>
+
+
+	<br />
+	<a href="session1.php">First page</a><br />
+	<a href="session3.php">Another page</a>
+<?php
 }else{
 ?>
 
---
+++ session3.php
@@ -0,0 +1,46 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Read the sessions
+if ($_SESSION["username"] != "") {
+	echo "<br>Hello ";
+    echo $_SESSION["username"];
+?>
+
+<h1>Valid User</h1>
+You are a valid user. Here is your reward!!!<br />
+
+<img src="magic-blacksmith-15511-1920x1080.JPG">
+
+
+<br /><br />
+<a href="clearsessions.php">Clear Sessions</a>
+
+<br />
+<a href="session1.php">Page 1</a>
+<br />
+<a href="session2.php">Page 2</a>
+
+
+
+<?php  
+}else{
+?>
+
+<h1>You are not a valid user</h1>
+Police are coming
+<img src="http://66.media.tumblr.com/tumblr_lne9okrwkN1qdvbl3o1_500.jpg"> <br />
+
+
+
+
+	<?php
+}
+?>
